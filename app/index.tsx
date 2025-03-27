b1706ea3ca4e393d5764a3a8fd06d4ed52d1bb65
Julia Pereira <vital.pereira.julia@gmail.com>
2025-03-27 14:43:19 -0300

    home Page

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import "../src/styles/global.css";
 
 import { StatusBar } from 'expo-status-bar'
-import { ImageBackground, Text, View } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 import {
   useFonts,
   Roboto_400Regular,
@@ -27,7 +27,7 @@ export default function App() {
 
   return (
     <ImageBackground source={blurBg} 
-    className="relative flex-1 items-center bg-gray-900"
+    className="relative px-8 py-10 flex-1 items-center bg-gray-900"
     imageStyle={{ position: 'absolute', left: '-100%' }}
     >
       <Stripes className="absolute left-2" />
@@ -37,7 +37,16 @@ export default function App() {
           <Text className="text-center font-title text-2xl leading-tight text-gray-50">Sua cápsula do tempo</Text>
           <Text className="text-center font-body text-base leading-relaxed text-gray-100">Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</Text>
         </View>
+
+        <TouchableOpacity 
+        activeOpacity={0.7}
+        className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">Cadastrar lembrança</Text>
+        </TouchableOpacity>
       </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">Feito com 💜 no NLW da Rocketseat</Text>
 
       <StatusBar style="light" translucent />
     </ImageBackground>
